988c089c91189e6c8feb3d08ede8b882c08c2fde
Aishik Saha <10260999+thecodeboy@users.noreply.github.com>
2024-01-30 21:50:52 +0530

    Modified homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 export default function Home() {
   return (
     <main>
-      <div className="antialiased">
-        <header className="flex flex-col items-start">
-          <a className="text-medium inline-block font-medium no-underline" href="/">Aishik Saha</a>
-          <span className="text-medium font-medium leading-none text-gray-500">Senior Software Engineer</span>
-        </header>
-      </div>
       <div className="mt-40">
         <h1>Hello there!</h1>
         I&apos;m a senior software engineer working at ServiceNow. I like building platforms and distributed systems.
