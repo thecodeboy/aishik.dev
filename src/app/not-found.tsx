c752a433e0ad294ccdbf9c892a5b48ec964e306f
Aishik Saha <10260999+thecodeboy@users.noreply.github.com>
2025-04-05 21:30:58 +0530

    Added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+    return (
+        <div className="topography-lines p-10 text-center mt-40 text-3xl font-extrabold rounded-full">
+            404 | This page could not be found.
+        </div>
+    )
+}
