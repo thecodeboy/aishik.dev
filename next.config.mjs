784e46b1bb7e9fab494cc6fdbe522362dfbdc031
Aishik Saha <10260999+thecodeboy@users.noreply.github.com>
2025-08-30 21:25:38 +0530

    Added Dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
